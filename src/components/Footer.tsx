8755ad641b21b842b048a2cc4b4ea277ad045d20
Weexy <146097877+Weexyy@users.noreply.github.com>
2024-04-04 11:58:28 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer(): JSX.Element {
 	return (
 		<footer className={styles["footer"]}>
 			&copy; {new Date().getFullYear()}{" "}
-			<a href="https://www.shangzhenyang.com/">{t("shangzhenYang")}</a>
+			<a href="https://weexy.cn">{t("Weexy")}</a>
 		</footer>
 	);
 }
